9cc0ffd9a28213f7f061bdc00ea975a6973439e9
CM <christofferjcm@gmail.com>
2019-11-22 10:14:34 +0100

    Add project files.

---
+++ ClassLibrary/ExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public static class ExtensionMethods
+    {
+        public static bool TryParse2(this string s, out int result)
+        {
+            bool isNum = false;
+            int num = 0;
+
+            foreach (char c in s)
+            {
+                if (char.IsDigit(c))
+                {
+                    num *= 10;
+                    num += c - '0';
+                    isNum = true;
+
+                    continue;
+                }
+                num = 0;
+                break;
+            }
+            
+            result = num;
+
+            return isNum;
+        }
+
+        public static (bool isNum, int num) TryParse2(this string s)
+        {
+            bool isNum = true;
+            int num = 0;
+
+            foreach (char c in s)
+            {
+                if (!char.IsDigit(c))
+                {
+                    isNum = false;
+                    num = 0;
+                    break;
+                }
+
+                num *= 10;
+                num += c - '0';
+            }
+
+            return (isNum, num);
+        }
+    }
+}
---
+++ TryParse2/Program.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary;
+using System;
+
+namespace TryParse2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter number: ");
+                string s = Console.ReadLine();
+
+                if (!s.TryParse2().isNum)
+                {
+                    Console.WriteLine("Not a number");
+                }
+                else
+                {
+                    Console.WriteLine("Result is: " + s.TryParse2().num);
+                }
+            }
+        }
+    }
+}
